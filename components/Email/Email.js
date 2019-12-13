0465a88d6d334fb70b89ea41cf8b07c0a92674c7
yossits <55515778+yossits@users.noreply.github.com>
2019-12-13 18:43:58 +0200

    fix email field

--- components/Email/Email.js
+++ components/Email/Email.js
@@ -11,8 +11,12 @@ export default class Email extends Component {
         if(event.target.value.indexOf('@') === 3) {
             this.setState({color: 'green'});
             this.props.callBack();
-        }else{
+        }
+        else if (event.target.value.length === 0){
             this.setState({color: ''});
+        }
+        else {
+            this.setState({color: 'red'});
         }
     }
     
